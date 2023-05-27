81a3393360d32aa801666905596d5cff050a3173
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-05-27 18:42:48 +0100

    add necessary methods

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -9,6 +9,6 @@ public class GameFrame extends JFrame {
         this.pack();
         this.setVisible(true);
         this.setLocationRelativeTo(null);
-        
+
     }
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,11 +1,40 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
 
 public class GamePanel extends JPanel implements ActionListener {
     GamePanel() {
 
     }
+    public void startGame() {
+
+    }
+    public void paintComponent(Graphics g) {
+
+    }
+    public void draw(Graphics g) {
+
+    }
+    public void move() {
+
+    }
+    public void checkPoint() {
+
+    }
+    public void checkCollisions() {
+
+    }
+    public void gameOver(Graphics g) {
+
+    }
+    public class MyKeyAdabter extends KeyAdapter {
+        public void keyPressed(KeyEvent e) {
+
+        }
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
