e7e92c38fdff3cd19c8b82368a84dee60815f64a
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-05-27 20:15:18 +0100

    the Snake is hungry 🐍

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -5,14 +5,14 @@ import java.util.Random;
 
 
 public class GamePanel extends JPanel implements ActionListener {
-    static final int SCREEN_WIDTH = 800;
-    static final int SCREEN_HEIGHT = 800;
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
     static final int DELAY = 75;
     final int x[] = new int[GAME_UNITS];
     final int y[] = new int[GAME_UNITS];
-    final int bodyParts = 6;
+    int bodyParts = 6;
     int pointsEarned;
     int pointX;
     int pointY;
@@ -22,38 +22,158 @@ public class GamePanel extends JPanel implements ActionListener {
     Random random;
 
     GamePanel() {
-
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdabter());
+        startGame();
     }
     public void startGame() {
-
+        newPoint();
+        running = true;
+        timer = new Timer(DELAY,this);
+        timer.start();
     }
     public void paintComponent(Graphics g) {
-
+        super.paintComponent(g);
+        draw(g);
     }
     public void draw(Graphics g) {
+        if(running){
+            for(int i = 0; i < (SCREEN_HEIGHT/UNIT_SIZE);i++){
+                g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
+                g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
+            }
+            g.setColor(Color.red);
+            g.fillOval(pointX,pointY,UNIT_SIZE,UNIT_SIZE);
+
+            for(int i = 0; i < (bodyParts); i++) {
+                if(i == 0) {
+                    g.setColor(Color.green);
+                    g.fillRect(x[i],y[i],UNIT_SIZE, UNIT_SIZE);
+                }else {
+                    g.setColor(new Color(45,180,0));
+                    g.fillRect(x[i],y[i],UNIT_SIZE, UNIT_SIZE);
+
+                }
+                g.setColor(Color.red);
+                g.setFont(new Font("Ink Free",Font.BOLD,45));
+                FontMetrics metrics = getFontMetrics(g.getFont());
+                g.drawString("Score: "+pointsEarned,(SCREEN_WIDTH - metrics.stringWidth("Score: "+pointsEarned))/2,g.getFont().getSize()/2);
+            }
+        }else {
+            gameOver(g);
+        }
+
+    }
+    public void newPoint() {
+        pointX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        pointY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
 
     }
     public void move() {
+        for(int i = bodyParts; i>0;i--){
+            x[i] = x[i-1];
+            y[i] = y[i-1];
 
+        }
+        switch(direction){
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+
+        }
     }
     public void checkPoint() {
-
+        if(x[0] == pointX && y[0] == pointY){
+            bodyParts++;
+            pointsEarned++;
+            newPoint();
+        }
     }
     public void checkCollisions() {
+        //Check if the head collides with body
+        for(int i = bodyParts; i > 0;i--) {
+            if(x[0] == x[i] && y[0] == y[i]){
+                running = false;
+            }
+        }
+        // check if head touches left border
+        if(x[0] < 0){
+            running = false;
+        }
+        // check if head touches right border
+        if(x[0] > SCREEN_WIDTH){
+            running = false;
+        }
+        //check if head touches top border
+        if(y[0] < 0) {
+            running = false;
+        }
+        // check if head touches bottom border
+        if(x[0] > SCREEN_HEIGHT){
+            running = false;
+        }
+        if(!running) {
+            timer.stop();
+        }
 
     }
     public void gameOver(Graphics g) {
+        g.setColor(Color.red);
+        g.setFont(new Font("Ink Free",Font.BOLD,75));
+        FontMetrics metrics = getFontMetrics(g.getFont());
+        g.drawString("Game Over !",(SCREEN_WIDTH - metrics.stringWidth("Game Over!"))/2,SCREEN_HEIGHT/2);
+
+
 
     }
     public class MyKeyAdabter extends KeyAdapter {
         public void keyPressed(KeyEvent e) {
+            switch(e.getKeyCode()){
+                case KeyEvent.VK_LEFT:
+                    if(direction != 'R'){
+                        direction = 'L';
+                    }
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if(direction != 'L') {
+                        direction = 'R';
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if(direction != 'U') {
+                        direction = 'D';
+                    }
+                    break;
+                case KeyEvent.VK_UP:
+                    if(direction != 'D'){
+                        direction = 'U';
+                    }
+                    break;
 
+            }
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if(running){
+            move();
+            checkPoint();
+            checkCollisions();
+        }
+        repaint();
     }
 }
 
