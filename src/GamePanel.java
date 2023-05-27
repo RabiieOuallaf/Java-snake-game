080d1f001e04d8e4ee853dffd18e600afb470d7c
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-05-27 18:49:22 +0100

    Add necessary properties

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -5,6 +5,22 @@ import java.util.Random;
 
 
 public class GamePanel extends JPanel implements ActionListener {
+    static final int SCREEN_WIDTH = 800;
+    static final int SCREEN_HEIGHT = 800;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
+    static final int DELAY = 75;
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    final int bodyParts = 6;
+    int pointsEarned;
+    int pointX;
+    int pointY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
+
     GamePanel() {
 
     }
